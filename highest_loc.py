652911dc5e848a8d62493811d6f41c2048634ff1
Ushasree Ginne <ushasree.ginne@gramener.com>
2016-08-29 13:47:25 +0000

    Replace highest_loc.py

--- highest_loc.py
+++ highest_loc.py
@@ -7,39 +7,53 @@ import sys
 
 
 def max_loc_data(*temp):
-	s1_out = subprocess.check_output([sys.executable, "project_details.py", "34"])
-	print s1_out
-	s2_out = subprocess.check_output([sys.executable, "loc.py", "34"])
-	print s2_out
+	#s1_out = subprocess.check_output([sys.executable, "project_details.py", "34"])
+	#print s1_out
+	#s2_out = subprocess.check_output([sys.executable, "loc.py", "34"])
+	#print s2_out
 	with open('loc_projects.csv','r') as csvfile:
 		reader = csv.reader(csvfile, delimiter='\t')
 		loc_max=0
 		max_data=[]
+		max_5=[[],[],[],[],[]]
+		loc_5=[0,0,0,0,0]
+		print len(loc_5)
+		#a= [[int(ro) for ro in row if ro.isdigit()] for row in reader]
 		for row in reader:
 			for r in row:
 				if r.isdigit():
 					a=int(r)
-			#print a
+			if all(i >= a for i in loc_5):
+				pass
+			else:
+				min_val=min(loc_5)
+				index=loc_5.index(min_val)
+				loc_5[index]=a
+				max_5[index]=row
+				
 			if loc_max<a:
 				#print loc_max,"  ",a
 				loc_max=a
 				max_data=row
 				pass
+		print max_5
+		print loc_5
 		completedata=[]
-		completedata.append([max_data[0],max_data[1]])
 		i=2
 		language=[]
 		number_of_lines=[]
 		while i<len(max_data)-1:
-			language.append(max_data[i].replace(":",""))
-			number_of_lines.append(int(max_data[i+1]))
+			_data=[]
+			_data.append(max_data[0])
+			_data.append(max_data[1])
+			_data.append(max_data[i].replace(":",""))
+			_data.append(int(max_data[i+1]))
 			i+=2
+			completedata.append(_data)
 			pass
-		completedata.append(language)
-		completedata.append(number_of_lines)
-		with open('data.txt', 'w') as outfile:
-			json.dumps(completedata, outfile)
-		print completedata
+		with open('loc.csv', 'wb') as csvfile:
+			writer = csv.DictWriter(csvfile, fieldnames = ["name", "project", "language","loc"],)
+			writer.writerows({'name': row[0], 'project': row[1], 'language': row[2],'loc':row[3] } for row in completedata)
 		return  json.dumps(completedata)
 	#return render_template("loc_plot.html",dataset=completedata)
 
