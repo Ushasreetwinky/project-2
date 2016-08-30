d240d1b8201ffea7a0ec11de8b17bfb460519ec9
Ushasree Ginne <ushasree.ginne@gramener.com>
2016-08-30 14:02:01 +0000

    Replace hack.py

--- hack.py
+++ hack.py
@@ -1,9 +1,26 @@
 from subprocess import Popen, PIPE
 import json
+import csv
 
 p = Popen("python new11.py", stderr=PIPE, stdout=PIPE, shell=True)
 output, errors = p.communicate()
 print  [errors, output]
+result=[]
+if output=="":
+	if errors.index("\n")>0:
+		er=errors.split("\n")
+		result.extend(er)
+	else:
+		result.append(errors.split("\n"))
+	pass
+else:
+	result.append(output)
+print result
+with open('ouput.csv', 'wb') as csvfile:
+	writer = csv.writer(csvfile, quotechar=" ", quoting=csv.QUOTE_MINIMAL)
+	writer.writerow(["output"])
+	for res in result:
+		writer.writerow([res])
 file = open("newfile.txt", "w")
 file.write(output)
 file.write(errors)
